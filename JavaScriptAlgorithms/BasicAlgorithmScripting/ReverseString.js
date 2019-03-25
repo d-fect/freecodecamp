9abf545591130a115c09ae9181007b6d70db6f90
Frank <frank@d-fect.com>
2019-03-25 13:31:41 +0100

    Added the smarter solution, using in-built javascript functions.

--- JavaScriptAlgorithms/BasicAlgorithmScripting/ReverseString.js
+++ JavaScriptAlgorithms/BasicAlgorithmScripting/ReverseString.js
@@ -1,9 +1,11 @@
 function reverseString(str) {
-    let reverseString = "";
-    for (let i = str.length; i > 0; i--) {
-        reverseString += str[i - 1];
-    }
-    return reverseString;
+    // let reverseString = "";
+    // for (let i = str.length; i > 0; i--) {
+    //     reverseString += str[i - 1];
+    // }
+    // return reverseString;
+
+    return str.split("").reverse().join("");
 }
 
 console.log(reverseString("Greetings from Earth"));

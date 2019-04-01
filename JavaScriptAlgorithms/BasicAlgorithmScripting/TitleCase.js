8894d5e78d853b22377686880d686e6938468509
Frank <frank@d-fect.com>
2019-04-01 14:45:54 +0200

    My *poor* solution for Title Case A Sentence.

--- JavaScriptAlgorithms/BasicAlgorithmScripting/TitleCase.js
+++ JavaScriptAlgorithms/BasicAlgorithmScripting/TitleCase.js
@@ -1,13 +1,16 @@
 function titleCase(str) {
     let arr = str.split(" ");
+    let first = '';
     for (let i = 0; i < arr.length; i++) {
         first = arr[i][0];
         first = first.toUpperCase();
-        console.log(first);
-        arr[i] = first;
+        arr[i] = first + arr[i].slice(1).toLowerCase();
     }
-    console.log(arr);
+    str = arr.join(" ");
+    console.log(str);
     return str;
 }
 
 titleCase("I'm a little tea pot");
+titleCase("sHoRt AnD sToUt");
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
